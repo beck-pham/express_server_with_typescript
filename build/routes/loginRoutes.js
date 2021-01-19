1c8982f4cd84af73a7e4b41dd7d4edac12be8d84
beck_pham <chauvupham@gmail.com>
2021-01-19 12:32:45 -0800

    implemented a basic type annotations within express app

--- build/routes/loginRoutes.js
+++ build/routes/loginRoutes.js
@@ -2,6 +2,14 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.router = void 0;
 var express_1 = require("express");
+function requireAuth(req, res, next) {
+    if (req.session && req.session.loggedIn) {
+        next();
+        return;
+    }
+    res.status(403);
+    res.send('Not Permitted');
+}
 var router = express_1.Router();
 exports.router = router;
 router.get('/login', function (req, res) {
@@ -18,4 +26,19 @@ router.post('/login', function (req, res) {
     else {
         res.send('Invalid email or password');
     }
+});
+router.get('/', function (req, res) {
+    if (req.session && req.session.loggedIn) {
+        res.send("\n      <div>\n        <h3>You are logged in </h3>\n        <a href=\"/logout\">Log Out</a>\n      </div>\n    ");
+    }
+    else {
+        res.send("\n      <div>\n        <h3>You are NOT logged in </h3>\n        <a href=\"/login\">Log In</a>\n      </div>\n    ");
+    }
+});
+router.get('/logout', function (req, res) {
+    req.session = undefined;
+    res.redirect('/');
+});
+router.get('/protected', requireAuth, function (req, res) {
+    res.send('You are in the protected route. Welcome users!');
 });
--- src/routes/loginRoutes.ts
+++ src/routes/loginRoutes.ts
@@ -1,11 +1,19 @@
-import { Router, Request, Response } from 'express';
+import { Router, Request, Response, NextFunction } from 'express';
 
 
 interface RequestWithBody extends Request {
   body: { [key: string]: string | undefined }
 }
 
+function requireAuth(req: Request, res: Response, next: NextFunction): void {
+  if(req.session && req.session.loggedIn) {
+    next();
+    return;
+  }
 
+  res.status(403);
+  res.send('Not Permitted');
+}
 
 
 
@@ -39,6 +47,33 @@ router.post('/login', (req: RequestWithBody, res: Response) => {
     res.send('Invalid email or password');
   }
 });
+
+router.get('/', (req: Request, res: Response) => {
+  if(req.session && req.session.loggedIn) {
+    res.send(`
+      <div>
+        <h3>You are logged in </h3>
+        <a href="/logout">Log Out</a>
+      </div>
+    `)
+  } else {
+    res.send(`
+      <div>
+        <h3>You are NOT logged in </h3>
+        <a href="/login">Log In</a>
+      </div>
+    `)
+  }
+})
+
+router.get('/logout', (req: Request, res: Response) => {
+  req.session = undefined;
+  res.redirect('/');
+})
+
+router.get('/protected', requireAuth, (req: Request, res: Response) => {
+  res.send('You are in the protected route. Welcome users!')
+})
 
 export { router };
 
