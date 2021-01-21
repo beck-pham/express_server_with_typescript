5a70cbb349857a9f89a91a0e08af1198b11239d8
beck_pham <chauvupham@gmail.com>
2021-01-21 13:04:53 -0800

    adding routerBinder to handle all REST API method

--- build/controllers/decorators/routes.js
+++ build/controllers/decorators/routes.js
@@ -1,11 +1,18 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.get = void 0;
+exports.patch = exports.del = exports.put = exports.post = exports.get = void 0;
 require("reflect-metadata");
-// factory decorator - A decorator that return a function
-function get(path) {
-    return function (target, key, desc) {
-        Reflect.defineMetadata('path', path, target, key);
+function routerBinder(method) {
+    // factory decorator - A decorator that return a function
+    return function (path) {
+        return function (target, key, desc) {
+            Reflect.defineMetadata('path', path, target, key);
+            Reflect.defineMetadata('method', method, target, key);
+        };
     };
 }
-exports.get = get;
+exports.get = routerBinder('get');
+exports.post = routerBinder('post');
+exports.put = routerBinder('put');
+exports.del = routerBinder('del');
+exports.patch = routerBinder('patch');
--- src/controllers/decorators/routes.ts
+++ src/controllers/decorators/routes.ts
@@ -1,8 +1,18 @@
 import 'reflect-metadata';
 
-// factory decorator - A decorator that return a function
-export function get(path: string) {
-  return function(target: any, key: string, desc: PropertyDescriptor) {
-    Reflect.defineMetadata('path', path, target, key);
+function routerBinder(method: string) {
+  // factory decorator - A decorator that return a function
+  return function(path: string) { 
+    return function(target: any, key: string, desc: PropertyDescriptor) {
+      Reflect.defineMetadata('path', path, target, key);
+      Reflect.defineMetadata('method', method, target, key)
+    }
   }
 }
+
+export const get = routerBinder('get');
+export const post = routerBinder('post');
+export const put = routerBinder('put');
+export const del = routerBinder('del');
+export const patch = routerBinder('patch');
+
